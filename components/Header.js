57830e507713752396f220b38c575d85e42de9e2
Augustin Berisa <34014106+yellowgh0st@users.noreply.github.com>
2021-11-30 03:42:39 +0100

    Remove docs

--- components/Header.js
+++ components/Header.js
@@ -66,16 +66,6 @@ export const Header = (props) => {
 							size={size}
 							variant='link'
 							lineHeight='normal'
-							onClick={() => open(defaults.url.docs)}
-							{...link}
-							{...style}
-						>
-					Docs
-						</Button>
-						<Button
-							size={size}
-							variant='link'
-							lineHeight='normal'
 							onClick={() => open(defaults.url.analytics)}
 							{...link}
 							{...style}
